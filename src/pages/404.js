f0f3ab04336dfccbf5858082834028410bd60137
mradic01 <mradi01@fesb.hr>
2020-10-14 00:05:07 +0200

    clean up

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,12 @@
 import React from "react"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Aux from '../hoc/auxiliary/auxiliary.js';
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
+  <Aux>
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+  </Aux>
 )
 
-export default NotFoundPage
+export default NotFoundPage;
