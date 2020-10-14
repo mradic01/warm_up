808cfbea2b6adbd2e5f12a064c545be9bb1c3c1b
mradic01 <mradi01@fesb.hr>
2020-10-14 02:04:40 +0200

    compatible with small devices

--- src/components/ContactBar/contactBar.js
+++ src/components/ContactBar/contactBar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import classes from './contactBar.module.css';
-import Aux from '../../hoc/auxiliary/auxiliary';
 
 import { AiFillPhone } from "react-icons/ai";
 import { SiMinutemailer } from "react-icons/si";
@@ -10,11 +9,11 @@ const contactBar = () => (
     <main className={classes.contactBar}>
         <div className={classes.contact}>
             <SiMinutemailer style={{marginRight: "5px"}}/>
-            <span>panicioJoni@gmail.com</span>
+            <span className={classes.hide}>panicioJoni@gmail.com</span>
         </div>
         <div className={classes.contact}>
             <AiFillPhone style={{marginRight: "5px"}}/>
-            <span>+385 95 874 8698</span>
+            <span className={classes.hide}>+385 95 874 8698</span>
         </div>
     </main>
 );
