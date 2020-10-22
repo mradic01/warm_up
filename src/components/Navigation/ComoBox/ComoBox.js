2ce93bf6ef1b4e31644bf251b8f8c60dbe5788e4
mradic01 <mradi01@fesb.hr>
2020-10-22 11:22:07 +0200

    Comobox added

---
+++ src/components/Navigation/ComoBox/ComoBox.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import classes from './comoBox.module.css';
+
+const comoBox = () => (
+    <span className={classes.comoBox}>ENGLISH</span>
+);
+
+export default comoBox;
--- src/components/Navigation/navigation.js
+++ src/components/Navigation/navigation.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import NavigationBar from './NavigationBar/navigationBar';
-import ComoBox from './ComoBox/ComoBox';
+import ComoBox from './ComoBox/comoBox';
+import classes from './navigation.module.css'
 
 const navigation = (props) => (
-    <div>
+    <div className={classes.navigation}>
         <NavigationBar activeBar={props.activeBar}/>
-        {/* <ComoBox/> */}
+        <ComoBox/>
     </div>
 );
 
