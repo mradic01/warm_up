abcd73d28ecd921a9a1a622263b782767cfa5205
mradic01 <mradi01@fesb.hr>
2020-10-25 18:23:13 +0100

    adding media query

--- src/components/Navigation/navigation.js
+++ src/components/Navigation/navigation.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
 import NavigationBar from './NavigationBar/navigationBar';
-import ComoBox from './ComoBox/comoBox';
 import classes from './navigation.module.css'
 
 const navigation = (props) => (
     <div className={classes.navigation}>
         <NavigationBar activeBar={props.activeBar}/>
-        <ComoBox/>
     </div>
 );
 
--- src/components/NavigationHeader/navigationHeader.js
+++ src/components/NavigationHeader/navigationHeader.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
 import classes from './navigationHeader.module.css';
-import Logo from '../Logo/logo'; 
+import Logo from '../Logo/logo';
 import Navigation from '../Navigation/navigation';
+import ComboBox from '../ComoBox/ComoBox';
 
 const navigationHeader = (props) => (
-    <section className={classes.NavigationHeader}>
-        <Logo/>
-        <Navigation activeBar={'HOME'}/>
-    </section>
+        <section className={classes.NavigationHeader}>
+            <Logo />
+            <Navigation activeBar={'HOME'} />
+            <ComboBox />
+        </section>
 );
 
 export default navigationHeader;
