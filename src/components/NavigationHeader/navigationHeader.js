3d13580e059930bca4ce06454a8b2989ec1f3efb
mradic01 <mradi01@fesb.hr>
2020-10-27 18:16:45 +0100

    adding ContactBar to NavigationHeader

--- src/components/NavigationHeader/navigationHeader.js
+++ src/components/NavigationHeader/navigationHeader.js
@@ -4,13 +4,17 @@ import classes from './navigationHeader.module.css';
 import Logo from '../Logo/logo';
 import Navigation from '../Navigation/navigation';
 import ComboBox from '../ComoBox/ComoBox';
+import ContactBar from '../ContactBar/contactBar';
 
 const navigationHeader = (props) => (
+    <>
+        <ContactBar />
         <section className={classes.NavigationHeader}>
             <Logo />
             <Navigation activeBar={'HOME'} />
             <ComboBox />
         </section>
+    </>
 );
 
 export default navigationHeader;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,11 @@ import contactBar from "../components/ContactBar/ContactBar";
 //import { Link } from "gatsby"
 
 import Aux from '../hoc/auxiliary/auxiliary';
-import ContactBar from '../components/ContactBar/contactBar';
 import NavigationHeader from '../components/NavigationHeader/navigationHeader';
 import Footer from "../components/Footer/footer";
 
 const IndexPage = () => (
   <Aux>
-    <ContactBar/>
     <NavigationHeader/>
     <Footer/>
   </Aux>
