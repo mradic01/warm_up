15d79cc52bf6e4b9a248f6e942572244e56978de
mradic01 <mradi01@fesb.hr>
2020-10-29 00:02:30 +0100

    styling InquiryBlock

--- src/components/InquiryBlock/inquiryBlock.js
+++ src/components/InquiryBlock/inquiryBlock.js
@@ -3,8 +3,22 @@ import React from 'react';
 import classes from './inquiryBlock.module.css';
 
 const inquiryBlock = () => (
-    <section className={classes}>
-
+    <section className={classes.inquiryBlock}>
+        <div className={classes.dateBoxContainer}>
+            <span>Arrival date</span>
+            <div className={classes.dateBox}>
+                <img src="https://github.com/n00ne1mportant/PublicFilesRepo/blob/master/appvd-calendar-2.png?raw=true" />
+                <span>{`${(new Date()).getDate()}.${(new Date()).getMonth() + 1}.${(new Date()).getFullYear()}`}</span>
+            </div>
+        </div>
+        <div className={classes.dateBoxContainer}>
+            <span>Departure date</span>
+            <div className={classes.dateBox}>
+                <img src="https://github.com/n00ne1mportant/PublicFilesRepo/blob/master/appvd-calendar-2.png?raw=true" />
+                <span>{`${(new Date()).getDate() + 1}.${(new Date()).getMonth() + 1}.${(new Date()).getFullYear()}`}</span>
+            </div>
+        </div>
+        <button>Send inquiry</button>
     </section>
 );
 
