43b6a56a9e8d69774a1425ae3ba05bc27607d042
mradic01 <mradi01@fesb.hr>
2020-10-28 23:41:41 +0100

    Styling TitleImage

--- src/components/TitleImage/titleImage.js
+++ src/components/TitleImage/titleImage.js
@@ -3,8 +3,11 @@ import React from 'react';
 import classes from './titleImage.module.css';
 
 const titleImage = () => (
-    <section className={classes}>
-
+    <section className={classes.titleImage}>
+        <div>
+            <h1>VILLA OLIVA VERDE</h1>
+            <p>Isolated from the tourist crowd for a pleasant<br /> and peaceful vacation</p>
+        </div>
     </section>
 );
 
