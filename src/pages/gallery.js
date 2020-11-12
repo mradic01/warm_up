a49c0e6a86c00dcbd379c1caa4bf616414abb2c8
mradic01 <mradi01@fesb.hr>
2020-11-12 03:51:09 +0100

    creating gallery page

---
+++ src/pages/gallery.js
@@ -0,0 +1,11 @@
+import React from "react"
+import HeaderFooterLayout from "../Layouts/headerFooterLayout";
+import SeparatorBar from '../modules/SeparatorBar/SeparatorBar';
+
+const PhotoGallery = () => (
+    <HeaderFooterLayout activeTab="Photo Gallery">
+        <SeparatorBar text="Photo Gallery" />
+    </HeaderFooterLayout>
+)
+
+export default PhotoGallery;
