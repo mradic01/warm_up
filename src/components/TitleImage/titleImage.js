618aa3f21e6ae04bfc5c90d68841a5c71fe0b77e
mradic01 <mradi01@fesb.hr>
2020-11-12 02:13:45 +0100

    Reaching titleImage with graphql

--- src/components/TitleImage/titleImage.js
+++ src/components/TitleImage/titleImage.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import BackgroundImage from "gatsby-background-image"
 
-import classes from './titleImage.module.css';
+import classes from "./titleImage.module.css"
 
-const titleImage = () => (
-    <section className={classes.titleImage}>
-        <div>
-            <h1>VILLA OLIVA VERDE</h1>
-            <p>Isolated from the tourist crowd for a pleasant<br /> and peaceful vacation</p>
-        </div>
-    </section>
-);
+const TitleImage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      desktop: file(relativePath: { eq: "titleImage.jpg" }) {
+        childImageSharp {
+          fluid(quality: 90, maxWidth: 1920) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <BackgroundImage
+      Tag="section"
+      className={classes.titleImage}
+      fluid={data.desktop.childImageSharp.fluid}
+      backgroundColor={`#040e18`}
+    >
+      <div>
+        <h1>VILLA OLIVA VERDE</h1>
+        <p>
+          Isolated from the tourist crowd for a pleasant
+          <br /> and peaceful vacation
+        </p>
+      </div>
+    </BackgroundImage>
+  )
+}
 
-export default titleImage;
+export default TitleImage
