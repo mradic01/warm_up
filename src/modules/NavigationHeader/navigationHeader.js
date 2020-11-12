8ddc952167042555cf5ca2cdd3ed45c223fe36a0
mradic01 <mradi01@fesb.hr>
2020-11-12 02:01:37 +0100

    react-select-flags implemented insted ComboBox

--- src/modules/NavigationHeader/navigationHeader.js
+++ src/modules/NavigationHeader/navigationHeader.js
@@ -3,8 +3,10 @@ import React from 'react';
 import classes from './navigationHeader.module.css';
 import Logo from '../../components/Logo/logo';
 import Navigation from '../../components/Navigation/navigation';
-import ComboBox from '../../components/ComoBox/ComoBox';
+// import ComboBox from '../../components/ComoBox/ComoBox';
 import ContactBar from '../../components/ContactBar/contactBar';
+import ReactFlagsSelect from 'react-flags-select';
+import 'react-flags-select/css/react-flags-select.css';
 
 const navigationHeader = (props) => (
     <>
@@ -12,7 +14,11 @@ const navigationHeader = (props) => (
         <section className={classes.NavigationHeader}>
             <Logo />
             <Navigation activeBar={props.activeTab} />
-            <ComboBox />
+            <ReactFlagsSelect countries={['GB', 'IT', 'DE']}
+                className={classes.combobox}
+                placeholder="ENGLISH"
+                customLabels={{ "GB": "English", "DE": "Deutsche", "IT": "Italiano" }}
+            />
         </section>
     </>
 );
